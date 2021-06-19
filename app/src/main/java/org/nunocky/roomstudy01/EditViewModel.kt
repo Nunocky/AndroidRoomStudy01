91b670a34bb59100b9e2fddedb6a879e69449746
Masato NUNOKAWA <masato.nunokawa@gmail.com>
2021-06-19 13:28:07 +0900

    非同期処理を ViewModelの状態変数で監視するのをやめる (#7)
    Fragmentに置いた方が処理の流れが直感的に把握できる

--- app/src/main/java/org/nunocky/roomstudy01/EditViewModel.kt
+++ app/src/main/java/org/nunocky/roomstudy01/EditViewModel.kt
@@ -2,35 +2,18 @@ package org.nunocky.roomstudy01
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import org.nunocky.roomstudy01.database.TopicRepository
-import org.nunocky.roomstudy01.database.room.Topic
 
-class EditViewModel(private val topicRepository: TopicRepository) : ViewModel() {
-    class Factory(private val topicRepository: TopicRepository) :
-        ViewModelProvider.NewInstanceFactory() {
-        @Suppress("unchecked_cast")
-        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-            return EditViewModel(topicRepository) as T
-        }
-    }
-
-    enum class Status {
-        Init,
-        Done
-    }
+class EditViewModel() : ViewModel() {
+//class EditViewModel(private val topicRepository: TopicRepository) : ViewModel() {
+//    class Factory(private val topicRepository: TopicRepository) :
+//        ViewModelProvider.NewInstanceFactory() {
+//        @Suppress("unchecked_cast")
+//        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+//            return EditViewModel(topicRepository) as T
+//        }
+//    }
 
     val ready = MutableLiveData(false)
     val title = MutableLiveData("")
-    val status = MutableLiveData(Status.Init)
 
-    fun updateTopic(topic: Topic) {
-        viewModelScope.launch(Dispatchers.IO) {
-            topicRepository.update(topic)
-            status.postValue(Status.Done)
-        }
-    }
 }
