8670cad914e9099281afa36f217cbbe3608d9251
Masato NUNOKAWA <masato.nunokawa@gmail.com>
2021-06-19 21:16:49 +0900

    リストアイテムの表示改善

--- app/src/main/java/org/nunocky/roomstudy01/utils/DataBindingAdapters.kt
+++ app/src/main/java/org/nunocky/roomstudy01/utils/DataBindingAdapters.kt
@@ -2,9 +2,12 @@ package org.nunocky.roomstudy01.utils
 
 import android.widget.ImageView
 import android.widget.ListView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import org.nunocky.roomstudy01.database.room.Topic
 import org.nunocky.roomstudy01.view.TopicListAdapter
+import java.text.SimpleDateFormat
+import java.util.*
 
 class DataBindingAdapters {
     companion object {
@@ -20,6 +23,15 @@ class DataBindingAdapters {
             if (newList != null) {
                 val adapter = listView.adapter as TopicListAdapter
                 adapter.updateItems(newList)
+            }
+        }
+
+        @JvmStatic
+        @BindingAdapter("date")
+        fun bindServerDate(textView: TextView, date: Date?) {
+            if (date != null) {
+                val fmt = SimpleDateFormat("yyyy/MM/dd hh:mm:ss")
+                textView.text = fmt.format(date)
             }
         }
     }
